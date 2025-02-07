8d856db0660ea6c65bf016622c8a88a29c5f9e2a
Elkana Kipruto <allanyegon443@gmail.com>
2025-02-07 16:23:49 +0300

    Relative container

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,8 +3,8 @@ import { FaCheck } from "react-icons/fa6";
 import BG from "./deliveryman.jpg"
 
 const Hero = () => (
-    <section id="home" className="md:grid md:grid-cols-2 md:gap-2 md:flex-row h-screen md:h-[70vh] lg:h-[50vh]">    
-        <div className="h-1/2 bg-main md:h-full md:col-span-1">
+    <section id="home" className="md:grid md:grid-cols-2 md:gap-2 md:flex-row flex-col h-screen md:h-[70vh] lg:h-[50vh]">    
+        <div className="relative h-1/2 bg-main md:h-full md:col-span-1">
           <div className="inset-0 top-1 text-left text-acc lg:p-8 lg:pt-16 lg:align-middle">
             <h1 className="p-2 m-1 font-semibold text-lg xs:p-1 xs:text-sm md:p-3 md:text-xl lg:text-3xl">BoderTact</h1>
             <p className="p-2 m-1 text-lg xs:p-1 xs:text-sm md:p-3 lg:text-2xl">Navigating customs regulations can be complex, but expert guidance ensures a smooth, compliant, and cost-effective supply chain.</p> 
@@ -15,7 +15,7 @@ const Hero = () => (
               </ul> 
           </div>
         </div>
-        <div className="h-1/2 object-cover md:col-span-1 md:h-full ">
+        <div className="relative h-1/2 bg-center object-cover md:col-span-1 md:h-full ">
           <img src= {BG} alt="Consultation" />
         </div>  
     </section>
